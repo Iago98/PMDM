9546cdf5f6432521e111bb3008f11e44d95e84d1
Iago98 <iagooterogoce@gmail.com>
2020-02-28 10:53:30 +0100

    uploading

--- P6/Ejercicio1/app/src/main/java/com/example/calculararea/ui/MainActivity.java
+++ P6/Ejercicio1/app/src/main/java/com/example/calculararea/ui/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
@@ -19,6 +21,7 @@ TextView wifiEna;
 TextView typeConn;
 TextView typeConn2;
 Switch boton;
+Button botonn;
     Connectivity conn = new Connectivity(this);
 
     @Override
@@ -48,40 +51,50 @@ Switch boton;
             }
 
         });
+        botonn = (Button) this.findViewById(R.id.button);
+        botonn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                conexion();
+            }
+        });
 
 
-        conexion();
 
 
 
     }
 
-    private void conexion(){
+    private void conexion() {
 
-        boolean isCon=conn.isConnected();
-        boolean isWif=conn.isWiFi();
+        boolean isCon = conn.isConnected();
+        boolean isWif = conn.isWiFi();
         typeConn2 = (TextView) this.findViewById(R.id.wifiAbajo);
         typeConn = (TextView) this.findViewById(R.id.datos);
         wifiEna = (TextView) this.findViewById(R.id.wifiArriba);
         if (isCon) {
             if (isWif) {
-                wifiEna.setBackgroundResource(R.drawable.wifi_1);
-                typeConn.setBackgroundResource(R.drawable.wifi_1_abajo);
-                typeConn2.setBackgroundResource(R.drawable.datos_0);
-            }else if(!isWif){
-                    typeConn.setBackgroundResource(R.drawable.wifi_0_abajo);
-                    typeConn2.setBackgroundResource(R.drawable.datos_1);
-            }else if(!isCon){
-                wifiEna.setBackgroundResource(R.drawable.wifi_1);
-                typeConn.setBackgroundResource(R.drawable.wifi_0_abajo);
-                typeConn2.setBackgroundResource(R.drawable.datos_0);
+                wifiEna.setBackgroundResource(R.drawable.wifi);
+                typeConn.setBackgroundResource(R.drawable.datos_0);
+                typeConn2.setBackgroundResource(R.drawable.wifi_1_abajo);
+                System.out.println("aqui1");
+            } else if (!isWif) {
+                wifiEna.setBackgroundResource(R.drawable.wifi);
+                typeConn.setBackgroundResource(R.drawable.datos_1);
+                typeConn2.setBackgroundResource(R.drawable.wifi_0_abajo);
+                System.out.println("aqui2");
+
 
             }
+        } else if (!isCon) {
+            System.out.println("aqui3");
+
+            wifiEna.setBackgroundResource(R.drawable.no_wifi);
+            typeConn.setBackgroundResource(R.drawable.datos_0);
+            typeConn2.setBackgroundResource(R.drawable.wifi_0_abajo);
+
         }
 
+
     }
-
-
-
-
 }
