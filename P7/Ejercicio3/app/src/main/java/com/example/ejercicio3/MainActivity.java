3bcae16b507ee33e2f4791b17c61a5e28bd9d2e5
Iago98 <iagooterogoce@gmail.com>
2020-03-14 21:58:56 +0100

    p7

--- P7/Ejercicio3/app/src/main/java/com/example/ejercicio3/MainActivity.java
+++ P7/Ejercicio3/app/src/main/java/com/example/ejercicio3/MainActivity.java
@@ -2,33 +2,95 @@ package com.example.ejercicio3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 
 
 public class MainActivity extends AppCompatActivity {
     Button btAdd;
+    Button back;
+    String texto;
     EditText editText;
-    String send;
+    WebView wvView;
+    private GestureDetector mDetector;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        back = (Button) this.findViewById(R.id.back);
          btAdd = (Button) this.findViewById(R.id.button);
-         editText=(EditText) this.findViewById(R.id.editText);
-        btAdd.setOnClickListener(new View.OnClickListener() {
+        editText=(EditText) this.findViewById(R.id.editText);
+        wvView = (WebView) this.findViewById( R.id.wvView );
+
+
+
+
+
+
+        back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                send="https://www."+editText.getText().toString();
-                Intent subActividad = new Intent(MainActivity.this, Search.class);
-                subActividad.putExtra("url", send);
-                MainActivity.this.startActivityForResult(subActividad, 100);
-            }
-        });
+                wvView.goBack();
 
+                                    }
+                                });
+        btAdd.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        System.out.println(editText.getText().toString());
+                        texto=editText.getText().toString();
+                        String part1="";
+                        String url="";
+
+                        try {
+                            String[] parts = texto.split("/");
+                             part1 = parts[0];
+
+
+
+
+                        }catch (Exception e){
+
+                        }
+
+
+                        if(part1.equalsIgnoreCase("https:")||part1.equalsIgnoreCase("http:")){
+                        url=editText.getText().toString();
+
+                        }else{
+                            url="https://www.google.com/search?q="+editText.getText().toString();
+                        }
+                        wvView.setWebViewClient(new WebViewClient());
+                        wvView.loadUrl(url);
+                        WebSettings wbSttngs = wvView.getSettings();
+                        wvView.requestFocus();
+                        wbSttngs.setJavaScriptEnabled(true);
+
+                        wbSttngs.setDisplayZoomControls(true);
+                    }
+                });
+
+
+    }
+
+
+
+
+    @Override
+    public void onBackPressed() {
+        if(wvView.canGoBack()){
+            wvView.goBack();
+        }else{
+            super.onBackPressed();
+        }
     }
 }
