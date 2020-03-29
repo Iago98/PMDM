680ccdb366eec3d8a87efffe4d73ec088538c4bb
Iago98 <iagooterogoce@gmail.com>
2020-03-29 22:39:13 +0200

    mejoras

--- P7/Ejercicio3/app/src/main/java/com/example/ejercicio3/MainActivity.java
+++ P7/Ejercicio3/app/src/main/java/com/example/ejercicio3/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     Button btAdd;
     Button back;
     String texto;
+    String texxt;
     EditText editText;
     WebView wvView;
     private GestureDetector mDetector;
@@ -48,7 +49,11 @@ public class MainActivity extends AppCompatActivity {
                     public void onClick(View v) {
                         System.out.println(editText.getText().toString());
                         texto=editText.getText().toString();
+                        texxt=editText.getText().toString();
                         String part1="";
+                        String texto2="";
+                        String texto3="";
+
                         String url="";
 
                         try {
@@ -57,18 +62,34 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+                        }catch (Exception e){
+
+                        }
+                        try {
+                            String[] parts2 = texxt.split("\\.");
+                            texto2 = parts2[0];
+                            texto3= parts2[2];
+
+
 
                         }catch (Exception e){
 
                         }
 
 
-                        if(part1.equalsIgnoreCase("https:")||part1.equalsIgnoreCase("http:")){
-                        url=editText.getText().toString();
+                        if(part1.equalsIgnoreCase("https:")||part1.equalsIgnoreCase("http:")) {
+                            url = editText.getText().toString();
+                        }else if(texto2.equalsIgnoreCase("www")){
+                            if(!texto3.equalsIgnoreCase("es")&&!texto3.equalsIgnoreCase("com")){
+                                url="https://www.google.com/search?q="+editText.getText().toString();
+                            }
 
-                        }else{
+                            url="https://"+editText.getText().toString();
+
+                            } else{
                             url="https://www.google.com/search?q="+editText.getText().toString();
                         }
+
                         wvView.setWebViewClient(new WebViewClient());
                         wvView.loadUrl(url);
                         WebSettings wbSttngs = wvView.getSettings();
